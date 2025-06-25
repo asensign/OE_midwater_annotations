3675be06b6f23510693c03734d93decaac5793b1
asensign <ensign.47@buckeyemail.osu.edu>
2025-06-25 16:30:20 -0400

    Update ROV_distance_traveled_IP.R

--- ROV_distance_traveled_IP.R
+++ ROV_distance_traveled_IP.R
@@ -32,6 +32,8 @@ ROV_distance_traveled_vec <- c() #this will become the vector of distances
 transect_times <- read.csv(paste0(transect_times_wd,"midwater_transect_times_",expedition,".csv"), header = TRUE)
 transect_times$start_UTC <-as.POSIXct(transect_times$start_UTC, tz = "UTC")
 transect_times$end_UTC<-as.POSIXct(transect_times$end_UTC, tz = "UTC")
+transect_depths <- data.frame(transect_times$transect_depth)
+# print(transect_depths)
 # print(transect_times)
 
 # dive number -- eventually this will be a loop (START IT HERE)
@@ -45,15 +47,22 @@ ROV_import_df <- ROV_import(paste0(ROV_filepath,expedition,"_DIVE",i,"_ROVtrack.
 # clean ROV df
 ROV_clean_df <- ROV_clean(ROV_import_df)
 #-------------------------------------------------------------------------------
-
 # filter ROV_clean_df for only times falling within transects (both unix now) (13 digits, but only 10 show - be careful)
+# Filter transect_times for only one dive at a time. 
 
-#only do one dive at a time from transect_times (they're lumped together by EX)
 ROV_transects <- data.frame()
 transect_times <- filter(transect_times, dive_number==sprintf("%s",ii))
 
-for(m in 1:nrow(ROV_clean_df)){                                             
-  for (n in 1:nrow(transect_times)){                                              
+# use unix time stamps to filter ROV_clean_df for annotations that fall between each transect, one transect at a time:
+
+for (n in 1:nrow(transect_times)){
+# for (n in 1:1){         
+  # for only the first transect of this dive
+  # Create a new empty datafame to hold filtered ROV data by transect.
+  #Will refresh for each transect as we loop thru n.
+  
+  # if ROV times from full ROV_clean_df fall within transect n, keep
+  for(m in 1:nrow(ROV_clean_df)){                                             
     ROV_time<-ROV_clean_df$unix_time[m]
 
     t_start<- transect_times$start_unix[n]                                 
@@ -63,16 +72,19 @@ for(m in 1:nrow(ROV_clean_df)){
       ROV_transects <- rbind((ROV_clean_df[m,]), ROV_transects)   
     }
   }
-}
-
-# add the EXACT 13-digit Unix times of transect start/end to the ROV_transect dataframe
-# add empty rows at the end
-# print(nrow(ROV_transects))
-ROV_transects[nrow(ROV_transects) + (nrow(transect_times)), ] <- NA
-print(nrow(ROV_transects))
-
+  
+  # View(ROV_transects)
+}  
+  
+ #-------------------------------------------------------------------------------
+# Add the EXACT 13-digit Unix times of transect start/end to the ROV_transect dataframe:
+  
+ # add empty rows at the end for later - these will contain transect start and end time
+# ROV_transect[nrow(ROV_transect) + (nrow(transect_times)), ] <- NA
+# ROV_transect[nrow(ROV_transect) + 2, ] <- NA
+  
 # Bind transect times and information with the ROV dataframe, for columns with the same datatype
-
+  
 start_rows <- data.frame(dplyr::select(transect_times, start_unix, lat_dd_start, lon_dd_start, expedition, dive_number, start_UTC, depth_m_start))
 end_rows <- data.frame(dplyr::select(transect_times, end_unix, lat_dd_end, lon_dd_end, expedition, dive_number, end_UTC, depth_m_end))
 
@@ -110,52 +122,61 @@ ROV_join <- dplyr::left_join(ROV_join1,
 ROV_join <- arrange(ROV_join, unix_time)
 
 # fill in transect depth all the way down. Not actual depth, but an ID for which transect we're on.
-ROV_join <- ROV_join %>% 
-  fill(transect_depth) 
+ROV_midwater <- ROV_join %>% 
+  fill(transect_depth) %>% 
+  dplyr::filter(!is.na(latitude_dd))
 
 # make sure all of that worked 
-# View(ROV_join)
+# View(ROV_midwater)
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 # create a loop by dive eventually on the outside (above)
 
-# create a loop by transect here
-# eventually, make this into a dynamic filter where you're grabbing all of the rows with same transect depth
-# for now, as a trial:
+# create a loop by transect 
+df_list <- list()
+  
+for (n in 1:length(transect_times$transect_depth)) {
+  
+  td <- as.numeric(transect_times$transect_depth[n])
+  df_name <- paste0("ROV_midwater_", td)
 
-ROV_benthic <- ROV_join %>% 
-  dplyr::filter(transect_depth=="800") %>% 
-  dplyr::filter(!is.na(latitude_dd))
-
+  df_list[[df_name]] <- ROV_midwater %>%
+    dplyr::filter(transect_depth == sprintf("%s",td))
   
   
-# ROV_benthic1 <- filter(ROV_join, transect_depth.x=="800") # rename transect-1 later
-# ROV_benthic <-  filter(ROV_benthic1, !is.na(latitude_dd)) # this gets rid of the transect times because they have no lat... SOLVED
+  write.csv(df_list[[df_name]], paste0(wd,"/exports/", expedition,"_dive", i, "_transect", td ,"_ROV_distance.csv"),row.names = FALSE)
+}
 
-print(nrow(ROV_benthic))
-View(ROV_benthic)
+# View(df_list$ROV_midwater_700)
 
+
+
+
+stop()
+# 6/25 working up to here. Issues with smoothing routine. Probably need to start from scratch
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
-
-
-# Now test out smoothing 
-ROV_SMA_window <- seq(from = 1, to = nrow(ROV_benthic), by = 100) # by 100 is arbitrary !
+#-------------------------------------------------------------------------------
+#Smoothing
+#iterate generation of smooths across full dataset, calculate distance traveled 
+#for each smooth, save into vector
+ROV_SMA_window <- seq(from = 1, to = nrow(ROV_benthic), by = 100)
+# print(ROV_SMA_window)
 ROV_SMA_distance <- c()
 
 for(j in ROV_SMA_window){
   ROV_smooth <- ROV_benthic |> 
-    dplyr::mutate(Lat_SMA = TTR::SMA(latitude_dd, n = j), # ttr is pkg, sma is simple moving avg
-                  Lon_SMA = TTR::SMA(longitude_dd, n = j), # smoothes lat/lon/depth cols
+    dplyr::mutate(Lat_SMA = TTR::SMA(latitude_dd, n = j),
+                  Lon_SMA = TTR::SMA(longitude_dd, n = j),
                   Depth_SMA = TTR::SMA(depth_m, n = j))
-  ROV_distance_smooth <- ROV_distance(ROV_smooth, lat = Lat_SMA, long = Lon_SMA)
+  ROV_distance_smooth <- ROV_distance(ROV_smooth, lat = Lat_SMA, long = Lon_SMA) 
   ROV_distance_m <- sum(ROV_distance_smooth$distance_3D_m, na.rm = TRUE)
   ROV_SMA_distance <- c(ROV_SMA_distance, ROV_distance_m)
 }
 
 #create data frame of smoothing window and total ROV distance traveled
 ROV_SMA_df <- as.data.frame(cbind(ROV_SMA_window, ROV_SMA_distance))
-
+View(ROV_distance_smooth)
 #-------------------------------------------------------------------------------
 #Outlier Detection
 
@@ -171,7 +192,7 @@ ROV_SMA_df_outliers <- as.data.frame(MadMed_out_dist[[3]])
 colnames(ROV_SMA_df_outliers) = c("distance")
 summary(ROV_SMA_df_outliers) #visual check
 
-outlier_threshold <- ROV_SMA_df |> # what is the point where you have the first non-outlier
+outlier_threshold <- ROV_SMA_df |> 
   dplyr::filter(!Distance_diff %in% ROV_SMA_df_outliers$distance) |> 
   dplyr::first()
 
@@ -179,13 +200,15 @@ ROV_distance_traveled <- outlier_threshold$ROV_SMA_distance
 
 ROV_distance_traveled_vec <- c(ROV_distance_traveled_vec, ROV_distance_traveled)
 
-print(paste0("Dive",i," completed"))
+# print(paste0("Dive",i," completed"))
 # }
 
 ROV_distance_df <- data.frame(expedition = expedition, 
                               dive_number = as.numeric(ROV_dive_numbers), 
                               distance_m = ROV_distance_traveled_vec)
 
+# write.csv(ROV_distance_df, paste0(wd,"/exports/", expedition,"_ROV_distance.csv"),
+          # row.names = FALSE)
 
 #-------------------------------------------------------------------------------
 #Visualize outlier detection results
@@ -203,11 +226,11 @@ ROV_distance_df <- data.frame(expedition = expedition,
 #Visualize raw and smoothed track lines
 
 ROV_smooth_predicted <- ROV_benthic |>
-  dplyr::mutate(Lat_SMA = TTR::SMA(latitude_dd, n = ROV_threshold$ROV_SMA_window),
-                Lon_SMA = TTR::SMA(longitude_dd, n = ROV_threshold$ROV_SMA_window),
-                Depth_SMA = TTR::SMA(depth_m, n = ROV_threshold$ROV_SMA_window))
-# 
-# #raw data
+  dplyr::mutate(Lat_SMA = TTR::SMA(latitude_dd), #, n = ROV_threshold$ROV_SMA_window),
+                Lon_SMA = TTR::SMA(longitude_dd), #, n = ROV_threshold$ROV_SMA_window),
+                Depth_SMA = TTR::SMA(depth_m)) #, n = ROV_threshold$ROV_SMA_window))
+
+#raw data
 # ROV_benthic |>
 #   leaflet::leaflet() |>
 #   leaflet::addTiles() |>
